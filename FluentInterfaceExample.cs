fe9bf80221a4d7d32b793c71da7c0bdf98608a0e
Federico Berasategui <fberasategui@lexim.com.ar>
2022-06-15 05:17:54 -0300

    Add files via upload

---
+++ FluentInterfaceExample.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace FluentInterfaceExample
+{
+    public class FluentExample1: IFluentInterface
+    {
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public void HiddenMethod1()
+        {
+
+        }
+
+        public void VisibleMethod1()
+        {
+
+        }
+    }
+
+    public interface FluentExample2: IFluentInterface
+    {
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public void HiddenMethod2()
+        {
+
+        }
+
+        public void VisibleMethod2()
+        {
+
+        }
+    }
+}
